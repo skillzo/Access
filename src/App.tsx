230f9e40b94e1ae4ee76c73096afb63bd97d1303
skillzo <skillzfire@yahoo.com>
2023-03-11 16:36:23 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -11,52 +11,66 @@ function App() {
 
   return (
     <>
-      <Wrapper>
-        {/* Navgation */}
-        <div className="flex items-center">
-          <BiArrowBack />
-          <p className="text-center text-lg font-medium w-[90%]">Transfer</p>
-        </div>
-
-        {/* Account Dashboard */}
-        <div className="bg-gradient-to-r from-[#4480e3] to-[#173f80] text-white text-xs p-[2em] rounded-lg  flex justify-between items-center">
-          <div className="space-y-2">
-            <div className="text-2xl font-semibold">
-              {showBalance ? "skillzo" : <p>&#8358; 0.10</p>}
-            </div>
-            <div>
-              {showBalance ? "PREMIER SAVINGS" : "PREMIER SAVINGS: SKILLZO"}
-            </div>
-            <div>Account Status: REGULAR</div>
+      <div className="hidden lg:flex h-screen w-full justify-center items-center text-[#ee585e] ">
+        This is a mobile app, Please switch to a Phone or a tablet
+      </div>
+      <div className="lg:hidden">
+        <Wrapper>
+          {/* Navgation */}
+          <div className="flex items-center">
+            <BiArrowBack />
+            <p className="text-center text-lg font-medium w-[90%]">Transfer</p>
           </div>
 
-          <div onClick={() => setShowBalance(!showBalance)}>
-            {showBalance ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
-          </div>
-        </div>
+          {/* Account Dashboard */}
+          <div className="bg-gradient-to-r from-[#4480e3] to-[#173f80] text-white text-xs p-[2em] rounded-lg  flex justify-between items-center">
+            <div className="space-y-2">
+              <div className="text-2xl font-semibold">
+                {showBalance ? "skillzo" : <p>&#8358; 0.10</p>}
+              </div>
+              <div>
+                {showBalance ? "PREMIER SAVINGS" : "PREMIER SAVINGS: SKILLZO"}
+              </div>
+              <div>Account Status: REGULAR</div>
+            </div>
 
-        <div className="text-xs text-slate-600 font-medium space-y-[0.5em]">
-          <p>Daily Transaction Limit: &#8358; 3,000,0000</p>
-          <div className="bg-[#dcdee3] ">
-            <div className="w-[20%] bg-[#173f80] p-[0.5em] rounded-r-full"></div>
+            <div onClick={() => setShowBalance(!showBalance)}>
+              {showBalance ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+            </div>
           </div>
 
           {/* transaction limit */}
-          <div>
-            <div className="flex items-center space-x-1">
-              <div className="w-[10px] h-[10px] bg-[#172f80] rounded-sm"></div>
-              <p>
-                <span className="text-[#173f80] font-semibold">
-                  &#8358;0.00
-                </span>
-                used
-              </p>
+          <div className="text-xs text-slate-600 font-medium space-y-[0.5em]">
+            <p>Daily Transaction Limit: &#8358; 3,000,0000</p>
+            <div className="bg-[#dcdee3] ">
+              <div className="w-[20%] bg-[#173f80] p-[0.5em] rounded-r-full"></div>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-1">
+                <div className="w-[10px] h-[10px] bg-[#172f80] rounded-sm"></div>
+                <p>
+                  <span className="text-[#173f80] font-semibold">
+                    &#8358;0.00
+                  </span>
+                  used
+                </p>
+              </div>
+              <div className="flex items-center space-x-1">
+                <div className="w-[10px] h-[10px] bg-[#172f80] rounded-sm"></div>
+                <p>
+                  <span className="text-[#173f80] font-semibold">
+                    &#8358;0.00
+                  </span>
+                  remaining
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <Outlet />
-      </Wrapper>
-      <Footer />
+          <Outlet />
+        </Wrapper>
+        <Footer />
+      </div>
     </>
   );
 }
