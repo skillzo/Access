32747a56370478aa5b19bbf7d3c4de79c3c7393f
skillzo <skillzfire@yahoo.com>
2023-06-26 23:43:44 +0100

    env depoloyment fix

--- src/store/Firebase/Firebase.js
+++ src/store/Firebase/Firebase.js
@@ -6,20 +6,13 @@ import { getFirestore } from "firebase/firestore";
 const config = import.meta.env;
 
 const firebaseConfig = {
-  // apiKey: config.VITE_REACT_APP_apikey,
-  // authDomain: config.VITE_REACT_APP_authDomain,
-  // projectId: "accessbank - c7361",
-  // storageBucket: config.VITE_REACT_APP_storageBucket,
-  // messagingSenderId: config.VITE_REACT_APP_messagingSenderId,
-  // appId: config.VITE_REACT_APP_appId,
-  // measurementId: config.VITE_REACT_APP_measurementId,
-  apiKey: "AIzaSyCSTkP00YgnCVcD8fmviKDxMME9go6Y10E",
-  authDomain: "accessbank-c7361.firebaseapp.com",
-  projectId: "accessbank-c7361",
-  storageBucket: "accessbank-c7361.appspot.com",
-  messagingSenderId: "1017817332911",
-  appId: "1:1017817332911:web:1720e2e51fdb1f5029285a",
-  measurementId: "G-4WNW20FFYS",
+  apiKey: config.VITE_REACT_APP_apikey,
+  authDomain: config.VITE_REACT_APP_authDomain,
+  projectId: config.VITE_REACT_APP_projectId,
+  storageBucket: config.VITE_REACT_APP_storageBucket,
+  messagingSenderId: config.VITE_REACT_APP_messagingSenderId,
+  appId: config.VITE_REACT_APP_appId,
+  measurementId: config.VITE_REACT_APP_measurementId,
 };
 
 // Initialize Firebase
